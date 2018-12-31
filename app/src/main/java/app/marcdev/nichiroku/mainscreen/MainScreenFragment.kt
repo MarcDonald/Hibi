852067b1e48ee638e9e4cc29e2c506a245b4db4e
Marc <marcsdeveloperemail@gmail.com>
2018-12-31 15:00:48 +0000

    Add ViewModel and ViewModelFactory for the Main Screen

--- app/src/main/java/app/marcdev/nichiroku/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/app/marcdev/nichiroku/mainscreen/MainScreenFragment.kt
@@ -5,20 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import app.marcdev.nichiroku.R
 import app.marcdev.nichiroku.internal.base.ScopedFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 import timber.log.Timber
 
 class MainScreenFragment : ScopedFragment(), KodeinAware {
 
+  // Kodein initialisation
   override val kodein by closestKodein()
 
+  // Viewmodel
+  private val viewModelFactory: MainScreenViewModelFactory by instance()
+  private lateinit var viewModel: MainScreenViewModel
+
+  // UI Components
   lateinit var loadingDisplay: ConstraintLayout
   lateinit var entriesRecycler: RecyclerView
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainScreenViewModel::class.java)
+  }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     Timber.d("Log: onCreateView: Started")
---
+++ app/src/main/java/app/marcdev/nichiroku/mainscreen/MainScreenViewModel.kt
@@ -0,0 +1,8 @@
+package app.marcdev.nichiroku.mainscreen
+
+import androidx.lifecycle.ViewModel
+import app.marcdev.nichiroku.data.repository.EntryRepository
+
+class MainScreenViewModel(private val entryRepository: EntryRepository) : ViewModel() {
+  // TODO
+}
