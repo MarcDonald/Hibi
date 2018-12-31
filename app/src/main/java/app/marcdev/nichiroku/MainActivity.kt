5b11e7017c1773e66f598d10ed7ced00f693fed5
Marc <marcsdeveloperemail@gmail.com>
2018-12-31 13:35:20 +0000

    Add base files and beginning of dependency injection
    * Add ScopedFragment and Delegates files
    * Put MainFragment into a package
    * Add the Application file and create Timber debug tree
    * Add beginning of dependency injection

--- app/src/main/java/app/marcdev/nichiroku/MainActivity.kt
+++ app/src/main/java/app/marcdev/nichiroku/MainActivity.kt
@@ -2,10 +2,12 @@ package app.marcdev.nichiroku
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    Timber.d("Log: onCreate: Started")
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
