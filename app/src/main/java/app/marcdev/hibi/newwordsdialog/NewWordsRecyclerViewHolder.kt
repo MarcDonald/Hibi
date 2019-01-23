9f56ed43622b4b21afe9589b80e374ef26fdd452
Marc <marcsdeveloperemail@gmail.com>
2019-01-23 13:20:12 +0000

    Hide New Word fields if empty

--- app/src/main/java/app/marcdev/hibi/newwordsdialog/NewWordsRecyclerViewHolder.kt
+++ app/src/main/java/app/marcdev/hibi/newwordsdialog/NewWordsRecyclerViewHolder.kt
@@ -21,12 +21,29 @@ class NewWordsRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemV
   fun display(newWord: NewWord) {
     displayedNewWord = newWord
 
-    wordDisplay.text = newWord.word
-    typeDisplay.text = newWord.partOfSpeech
-    readingDisplay.text = newWord.reading
-    englishDisplay.text = newWord.english
-    notesDisplay.text = newWord.notes
+    if(newWord.word.isNotBlank())
+      wordDisplay.text = newWord.word
+    else
+      wordDisplay.visibility = View.GONE
 
-    // TODO logic for when to display certain ones
+    if(newWord.partOfSpeech.isNotBlank())
+      typeDisplay.text = newWord.partOfSpeech
+    else
+      typeDisplay.visibility = View.GONE
+
+    if(newWord.reading.isNotBlank())
+      readingDisplay.text = newWord.reading
+    else
+      readingDisplay.visibility = View.GONE
+
+    if(newWord.english.isNotBlank())
+      englishDisplay.text = newWord.english
+    else
+      englishDisplay.visibility = View.GONE
+
+    if(newWord.notes.isNotBlank())
+      notesDisplay.text = newWord.notes
+    else
+      notesDisplay.visibility = View.GONE
   }
 }
