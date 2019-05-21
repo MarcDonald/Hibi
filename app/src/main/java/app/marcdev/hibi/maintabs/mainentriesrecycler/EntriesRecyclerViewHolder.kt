7ff939ea42c0c986fe843392e055fa21ba2d5cf3
Marc <marcsdeveloperemail@gmail.com>
2019-05-21 12:16:50 +0100

    Display location on Main Screen
    * Also made the Notifications preference category header use a string
    resource rather than hard coding it

--- app/src/main/java/app/marcdev/hibi/maintabs/mainentriesrecycler/EntriesRecyclerViewHolder.kt
+++ app/src/main/java/app/marcdev/hibi/maintabs/mainentriesrecycler/EntriesRecyclerViewHolder.kt
@@ -1,6 +1,7 @@
 package app.marcdev.hibi.maintabs.mainentriesrecycler
 
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.navigation.Navigation
 import app.marcdev.hibi.R
@@ -11,13 +12,17 @@ import com.google.android.material.chip.ChipGroup
 
 class EntriesRecyclerViewHolder(itemView: View) : BaseEntriesRecyclerViewHolder(itemView) {
 
-  // Entry
-  private var displayedItem: MainEntriesDisplayItem? = null
-
-  // UI Components
+  // <editor-fold desc="UI Components">
   private var dateTimeDisplay: TextView = itemView.findViewById(R.id.item_date_time)
   private var contentDisplay: TextView = itemView.findViewById(R.id.item_content)
-  private var chipGroup: ChipGroup = itemView.findViewById(R.id.cg_main_tags)
+  private var tagChipGroup: ChipGroup = itemView.findViewById(R.id.cg_main_tags)
+  private var tagDisplay: LinearLayout = itemView.findViewById(R.id.lin_main_tags)
+  private var locationDisplay: TextView = itemView.findViewById(R.id.txt_item_location)
+  // </editor-fold>
+
+  // <editor-fold desc="Other">
+  private var displayedItem: MainEntriesDisplayItem? = null
+  // </editor-fold>
 
   private val clickListener = View.OnClickListener { view ->
     val viewEntryAction = MainScreenFragmentDirections.viewEntryAction()
@@ -32,19 +37,37 @@ class EntriesRecyclerViewHolder(itemView: View) : BaseEntriesRecyclerViewHolder(
   }
 
   override fun display(item: MainEntriesDisplayItem) {
+    this.displayedItem = item
     val dateTimeDisplayText = formatDateTimeForDisplay(item.entry.day, item.entry.month, item.entry.year, item.entry.hour, item.entry.minute)
     dateTimeDisplay.text = dateTimeDisplayText
     contentDisplay.text = item.entry.content
+    displayLocation()
+    displayTags()
+  }
 
-    chipGroup.removeAllViews()
-    if(item.tags.isNotEmpty()) {
-      item.tags.forEach {
-        val chip = Chip(itemView.context)
-        chip.text = it
-        chipGroup.addView(chip)
+  private fun displayLocation() {
+    displayedItem?.let { item ->
+      if(item.entry.location.isNotBlank()) {
+        locationDisplay.text = item.entry.location
+        locationDisplay.visibility = View.VISIBLE
+      } else {
+        locationDisplay.visibility = View.GONE
       }
     }
+  }
 
-    this.displayedItem = item
+  private fun displayTags() {
+    displayedItem?.let { item ->
+      tagDisplay.visibility = if(item.tags.isEmpty()) View.GONE else View.VISIBLE
+
+      tagChipGroup.removeAllViews()
+      if(item.tags.isNotEmpty()) {
+        item.tags.forEach {
+          val chip = Chip(itemView.context)
+          chip.text = it
+          tagChipGroup.addView(chip)
+        }
+      }
+    }
   }
 }
