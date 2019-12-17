18dced70c7d4cc525cdda591c1dd404252d0036a
Marc <marcsdeveloperemail@gmail.com>
2019-12-17 20:34:18 +0000

    Update dependencies

--- app/src/main/java/com/marcdonald/hibi/screens/about/AboutPreferenceFragment.kt
+++ app/src/main/java/com/marcdonald/hibi/screens/about/AboutPreferenceFragment.kt
@@ -34,40 +34,40 @@ class AboutPreferenceFragment : PreferenceFragmentCompat() {
 	private fun bindViews() {
 		initVersion()
 
-		findPreference("about_oss").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+		findPreference<Preference>("about_oss")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
 			val ossAction = AboutFragmentDirections.ossAction()
 			Navigation.findNavController(requireView()).navigate(ossAction)
 
 			true
 		}
 
-		findPreference("about_jisho").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+		findPreference<Preference>("about_jisho")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
 			launchURL("https://jisho.org/")
 			true
 		}
 
-		findPreference("about_privacy").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+		findPreference<Preference>("about_privacy")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
 			val privacyAction = AboutFragmentDirections.privacyAction()
 			Navigation.findNavController(requireView()).navigate(privacyAction)
 			true
 		}
 
-		findPreference("about_code").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+		findPreference<Preference>("about_code")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
 			launchURL("https://github.com/MarcDonald/Hibi")
 			true
 		}
 
-		findPreference("about_author").onPreferenceClickListener = Preference.OnPreferenceClickListener {
+		findPreference<Preference>("about_author")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
 			launchURL("https://marcdonald.com")
 			true
 		}
 	}
 
 	private fun initVersion() {
-		val version = findPreference("about_version")
-		version.summary = BuildConfig.VERSION_NAME
-		version.setOnPreferenceClickListener {
-			val buildCodeString = Integer.toString(BuildConfig.VERSION_CODE)
+		val version = findPreference<Preference>("about_version")
+		version?.summary = BuildConfig.VERSION_NAME
+		version?.setOnPreferenceClickListener {
+			val buildCodeString = BuildConfig.VERSION_CODE.toString()
 			Toast.makeText(requireContext(), resources.getString(R.string.build_code_with_output, buildCodeString), Toast.LENGTH_SHORT).show()
 			true
 		}
